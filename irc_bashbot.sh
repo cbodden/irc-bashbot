6c0dc6d6424ffabd632a08bcaf76f24af3376251
cesar <cesar@pissedoffadmins.com>
2016-10-15 21:32:33 +0000

    adding pw support for direct message

--- irc_bashbot.sh
+++ irc_bashbot.sh
@@ -10,7 +10,7 @@
 #        AUTHOR: cesar@pissedoffadmins.com
 #  ORGANIZATION: pissedoffadmins.com
 #       CREATED: 14 Oct 2016
-#      REVISION: 4
+#      REVISION: 5
 #===============================================================================
 
 ## sourcing config file
