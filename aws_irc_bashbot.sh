cf6db54ca7726c5d73c9f833b91fbbed61fa0a15
cesar <cesar@pissedoffadmins.com>
2016-10-15 03:52:21 +0000

    changes: - started separating files into core and lib - fixed garbage recv && send - added pong loop to prevent 260 seconds timeout - colorized stout - fixed trap

--- aws_irc_bashbot.sh
+++ aws_irc_bashbot.sh
@@ -17,58 +17,28 @@
 #      REVISION: 1
 #===============================================================================
 
-LC_ALL=C
-LANG=C
-set -o pipefail
-set -o nounset
-set -o errexit
-
-PROGNAME=$(basename $0)
-PROGDIR=$(readlink -m $(dirname $0))
-
-ORN=$(tput setaf 3)
-RED=$(tput setaf 1)
-BLU=$(tput setaf 4)
-GRN=$(tput setaf 40)
-CLR=$(tput sgr0)
-MNHDR="${BLU}[*]${CLR} "
-BDHDR="${RED}[*]${CLR} "
-COLHDR="${GRN}[*]${CLR} "
-
-if [[ $(uname) != "Linux" ]]
-then
-    printf "%s\n" "${RED}Needs Linux${CLR}"
-    exit 1
-fi
-
-trap 'rm -rf ${TMP_FILE1 ${TMP_FILE2} ; exit' 0 1 2 3 9 15
-
-TMP_FILE1=$(mktemp --tmpdir ${PROGNAME}.$$.XXXXXXXXXX)
-TMP_FILE2=$(mktemp --tmpdir ${PROGNAME}.$$.XXXXXXXXXX)
-
-if [[ -r ${PROGNAME}.config ]]
-then
-    source ${PROGNAME}.config
-else
-    printf "%s\n" "${PROGNAME}.config not found!!"
-fi
+source aws_irc_bashbot.config
+source core/main.shlib
+main
 
 # Basic helpers for communication with via IRC protocol
 function RECV()
 {
-    ${ORN}
-    echo "< $@" >&2
-    ${CLR}
+    echo -n "${ORN}"
+    echo "< $@"
+    # echo "< $@" >&2
+    echo -n "${CLR}"
 }
 
 function SEND()
 {
-    ${BLU}
-    echo "> $@" >&2;
+    echo -n "${BLU}"
+    echo "> $@"
+    # echo "> $@" >&2
     printf "%s\r\n" "$@" >&3;
-    ${CLR}
+    echo -n "${CLR}"
 }
-export -f RECV
+# export -f RECV
 export -f SEND
 
 exec 3<>/dev/tcp/${SERVER}/${PORT} \
@@ -101,33 +71,26 @@ do
 
     case "$@" in
         "PING "*)
-            SEND "PONG $2"
+            _SERVER="$2"
+            while true
+            do
+                SEND "PONG ${_SERVER}"
+                sleep 180
+            done &
             continue
             ;;
         "PRIVMSG "*" :"*)
             CHANNEL=$2
             CMD=${3#:}
             ARG=${@:4}
-            while IFS= read -r LINE; do
+            if [[ ${CMD} = .* ]]
+            then
                 if [[ ${CMD} == ".aws" ]]
                 then
-                    aws ec2 describe-instances \
-                        --output text \
-                        --query 'Reservations[*].Instances[*].[InstanceId,KeyName,InstanceType,State.Name]' > ${TMP_FILE1}
-                    cat ${TMP_FILE1} | tr '\t' ' ' | column -t > ${TMP_FILE2}
-                    _CNT=1
-                    _FCNT=$(wc -l ${TMP_FILE2} | awk '{print$1}')
-                    SEND "PRIVMSG ${CHANNEL} :Here is your requested info ${NAME}:"
-                    while [[ "${_CNT}" -le "${_FCNT}" ]]
-                    do
-                        _OUT=$(sed -n ${_CNT}p ${TMP_FILE2})
-                        SEND "PRIVMSG ${CHANNEL} :${_OUT}"
-                        let _CNT=_CNT+1
-                        sleep .25
-                    done
-                    SEND "PRIVMSG ${CHANNEL} :Here is ARG: ${ARG}"
+                    source lib/aws.shlib
+                    .aws
                 fi
-            done&
+            fi
             continue
             ;;
         *)
