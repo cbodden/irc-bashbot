dd614ce7c1d35abc06e25b3021ebea90c02704df
cesar <cesar@pissedoffadmins.com>
2016-10-31 15:41:51 +0000

    rev bump

--- irc_bashbot.sh
+++ irc_bashbot.sh
@@ -10,7 +10,7 @@
 #        AUTHOR: cesar@pissedoffadmins.com
 #  ORGANIZATION: pissedoffadmins.com
 #       CREATED: 14 Oct 2016
-#      REVISION: 18
+#      REVISION: 19
 #===============================================================================
 
 ## check and source config file
