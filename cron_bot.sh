07006db330fafea2b3105ec07f3f60ed9fd4b9e5
cbodden <cesar@pissedoffadmins.com>
2017-06-08 17:57:49 -0400

    initial add

---
+++ cron_bot.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+## this script is a hack fix for a weird issue where the irc bot keeps getting
+## killed off at standard intervals.
+
+set -o nounset
+
+for ITER in $(\
+    ps -ef \
+    | awk '/irc_bashbot.sh$/ {print $2}')
+do
+    kill -9 ${ITER}
+done
+
+sleep 5
+
+~/git/mine/irc-bashbot/irc_bashbot.sh
